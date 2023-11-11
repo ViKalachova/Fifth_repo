101b87a98d8545f9440ed3cfe7439631506a35d7
ViKalachova <kalachova.vs@gmail.com>
2023-11-11 19:37:21 +0200

    second commit

--- main_my.py
+++ main_my.py
@@ -56,3 +56,28 @@ class AddressBook(UserDict):
         for key in list(self.data.keys()):
             if str(key) == name:
                 del self.data[key]
+
+
+
+
+if __name__ == '__main__':
+    book = AddressBook()
+    john_record = Record("John")
+    john_record.add_phone("1234567890")
+    john_record.add_phone("5555555555")
+    book.add_record(john_record)
+    jane_record = Record("Jane")
+    jane_record.add_phone("9876543210")
+    book.add_record(jane_record)
+    for name, record in book.data.items():
+        print(name, record)
+    john = book.find("John")
+    john.edit_phone("1234567890", "1112223333")
+    print(john)
+    found_phone = john.find_phone("5555555555")
+    print(f"{john.name}: {found_phone}")
+    book.delete("Jane")
+    print(book)
+
+
+
